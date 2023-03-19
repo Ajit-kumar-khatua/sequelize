b4841af4c729879dfea81d0461d8800c3d2467dc
unknown <ajitkumarkhatua286@gmail.com>
2023-03-19 14:50:38 +0530

    database added

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@
 const { Sequelize , DataTypes }=require("sequelize")
 require("dotenv").config
 
-const sequelize=new Sequelize("evaluation1","admin",process.env.password,{
-    host: "localhost",
+const sequelize=new Sequelize("products","admin","khatua456",{
+    host: "database-1.cxgjsqtla1vy.ap-northeast-1.rds.amazonaws.com",
     dialect: "mysql"
 })
 
